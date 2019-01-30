f3d01f0d253f8752ee85f5ad5c2f027447539d68
山田 俊輔 <yshunske@gmail.com>
2019-01-30 23:59:24 +0900

    Elasticsearchのhost名をdocker-composeで指定したhost名に修正

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 config = {
-    host:  ENV['ELASTICSEARCH_HOST'] || "http://localhost:9200/",
+    host:  ENV['ELASTICSEARCH_HOST'] || "es:9200/",
 }
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
