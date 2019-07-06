b41174456fdb3da60fc9de4b8eb14e6da9827d3c
山田 俊輔 <yshunske@gmail.com>
2019-07-06 23:40:15 +0900

    サジェストを返却するapi追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :mangas
+  resources :mangas do
+    collection do
+      get :suggest
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
