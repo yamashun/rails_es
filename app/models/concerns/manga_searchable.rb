aae840dcbb9406a07cb0a85934b70c9b8f47abae
山田 俊輔 <yshunske@gmail.com>
2019-07-06 15:00:53 +0900

    未使用コード削除

--- app/models/concerns/manga_searchable.rb
+++ app/models/concerns/manga_searchable.rb
@@ -6,13 +6,13 @@ module MangaSearchable
 
     index_name "es_manga_#{Rails.env}"
 
-    settings analysis: self.analyzer_settings do
+    settings do
       mappings dynamic: 'false' do
         indexes :id,                   type: 'integer'
         indexes :publisher,            type: 'keyword'
         indexes :author,               type: 'keyword'
         indexes :category,             type: 'text', analyzer: 'kuromoji'
-        indexes :title,                type: 'text', analyzer: 'katakana_analyzer'
+        indexes :title,                type: 'text', analyzer: 'kuromoji'
         indexes :description,          type: 'text', analyzer: 'kuromoji'
       end
     end
@@ -53,39 +53,13 @@ module MangaSearchable
       __elasticsearch__.search({
         query: {
           multi_match: {
-            fields: %w(id publisher author category title),
+            fields: %w(id publisher author category title description),
             type: 'cross_fields',
-            analyzer: 'katakana_analyzer',
             query: query,
             operator: "and"
           }
         }
       })
-    end
-
-    # TODO それぞれのフィールドに対して検索を行う。重みづけが可能か調査。    
-    def manga_search_each_field
-    end
-
-    def analyzer_settings
-      {
-        analyzer: {
-          katakana_analyzer: {
-            tokenizer: 'kuromoji_tokenizer',
-            filter: ['katakana_readingform', 'hiragana_to_katakana']
-          }
-        },
-        filter: {
-          katakana_readingform: {
-            type: 'kuromoji_readingform',
-            use_romaji: false
-          },
-          hiragana_to_katakana: {
-            type: 'icu_transform',
-            id: 'Hiragana-Katakana'
-          }
-        }
-      }
     end
   end
 end
