ce53e3071a7c123d9d0a2808614aa626e23f1326
山田 俊輔 <yshunske@gmail.com>
2019-07-14 16:50:32 +0900

    Revert "検索履歴保存用のテーブル追加"
    This reverts commit 2b76e761078d4413c97aaef39edf397a9a2ddef7.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_132134) do
+ActiveRecord::Schema.define(version: 2019_01_27_114207) do
 
   create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -35,13 +35,6 @@ ActiveRecord::Schema.define(version: 2019_06_01_132134) do
 
   create_table "publishers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-  end
-
-  create_table "search_word_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "word"
-    t.integer "hit_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "mangas", "authors"
